0b774ae101d076db74bf04525ab323d94756904d
singsing215 <felvex@163.com>
2020-07-10 18:24:12 +0800

    defaultProps

--- my-react-app/src/KeyConcept/propesTypee.jsx
+++ my-react-app/src/KeyConcept/propesTypee.jsx
@@ -9,9 +9,11 @@ export default class propesTypee extends React.Component {
                 2. 规范数据类型，增强程序的健壮性 <br />
                 3. 类型：array,bool,func,number,object,string,symbol <br/>
                 4. import PropTypes from 'prop-types' <br />
-                5. 通过propTypes验证title: PropTypes.string <br/>
-                6. 必选项：title: PropTypes.string.isRequired <br/>
-                6. App.jsx注释 <br/>
+                5. 类型检查: 类名.propTypes <br/>
+                6. 通过propTypes验证：title: PropTypes.string <br/>
+                7. 必选项：title: PropTypes.string.isRequired <br/>
+                8. 设置默认props：类名.defaultProps <br/>
+                9. App.jsx注释 <br/>
                 {this.props.title}
             </div>
         )
@@ -21,4 +23,9 @@ export default class propesTypee extends React.Component {
 
 propesTypee.propTypes = {
     title: PropTypes.string
+};
+
+
+propesTypee.defaultProps = {
+    title: "默认值"
 };
