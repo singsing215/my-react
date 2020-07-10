09fca59e677a9db13dd42b844cbaf8bd2b63d1a7
singsing215 <felvex@163.com>
2020-07-10 13:58:57 +0800

    child2parent

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import component from "./KeyConcept/component";
 import props from "./KeyConcept/props";
 import state from "./KeyConcept/state";
 import lifecycle from "./KeyConcept/lifecycle";
+import Lifecycle from "./KeyConcept/lifecycle"; //props修改：在父App.jsx修改props,传title给lifecycle.jsx组件
 import setstate from "./KeyConcept/setState";
 import iff from "./KeyConcept/if";
 import listkey from "./KeyConcept/list&key";
@@ -19,35 +20,64 @@ import form2 from "./KeyConcept/form2";
 import propsTypee from "./KeyConcept/propesTypee";
 
 
-const nav1 = ["1","2","3"]//多个导航，App.js传送两个nav，在Nav.jsx接收两个nav
-const nav2 = ["4","5","6"]
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        {/* <Nav nav={ nav1 } title="主导航"/>
+export default class App extends React.Component {
+
+  //生命周期函数修改props,传title给lifecycle组件
+  constructor() {
+    super();
+    this.state = {
+      title: "props"
+    }
+  }
+
+  parent2child=()=>{
+    this.setState({
+      title:"父props传子"
+    })
+  }
+
+  child2parent=(data)=>{
+    this.setState({
+      title:data  //data   //"props已修改"
+    })
+  }
+
+  render() {
+
+    // //props
+    const nav1 = ["1", "2", "3"]//多个导航，App.js传送两个nav，在Nav.jsx接收两个nav
+    const nav2 = ["4", "5", "6"]
+
+    return (
+      <div className="App">
+        <Router>
+          {/* props */}
+          {/* <Nav nav={ nav1 } title="主导航"/>
         <Nav nav={ nav2 } title="副导航"/> */}
-        <Nav />
-        <Route path="/render" component={render}></Route>
-        <Route path="/component" component={component}></Route>
-        <Route path="/props" component={props}></Route>
-        <Route path="/state" component={state}></Route>
-        <Route path="/lifecycle" component={lifecycle}></Route>
-        <Route path="/setstate" component={setstate}></Route>
-        <Route path="/if" component={iff}></Route>
-        <Route path="/listkey" component={listkey}></Route>
-        <Route path="/parent" component={parent}></Route>
-        <Route path="/child1" component={child1}></Route>
-        <Route path="/child2" component={child2}></Route>
-        <Route path="/compose" component={compose}></Route>
-        <Route path="/form1" component={form1}></Route>
-        <Route path="/refs" component={refs}></Route>
-        <Route path="/form2" component={form2}></Route>
-        <Route path="/propsTypee" component={propsTypee}></Route>
-      </Router>
-    </div>
-  );
+          <Nav />
+          <Route path="/render" component={render}></Route>
+          <Route path="/component" component={component}></Route>
+          <Route path="/props" component={props}></Route>
+          <Route path="/state" component={state}></Route>
+          <Route path="/lifecycle" component={lifecycle}></Route>
+          <Route path="/setstate" component={setstate}></Route>
+          <Route path="/if" component={iff}></Route>
+          <Route path="/listkey" component={listkey}></Route>
+          <Route path="/parent" component={parent}></Route>
+          <Route path="/child1" component={child1}></Route>
+          <Route path="/child2" component={child2}></Route>
+          <Route path="/compose" component={compose}></Route>
+          <Route path="/form1" component={form1}></Route>
+          <Route path="/refs" component={refs}></Route>
+          <Route path="/form2" component={form2}></Route>
+          <Route path="/propsTypee" component={propsTypee}></Route>
+        </Router>
+        {/* 父传子 */}
+        {/* <Lifecycle title={this.state.title} child2parent={this.child2parent}/>
+        props修改：在父App.jsx修改props,传title给lifecycle.jsx组件(父传子) <br/>
+        <button onClick={this.parent2child}>父传子修改props</button> */}
+      </div>
+    );
+  }
 }
-
-export default App;
--- my-react-app/src/KeyConcept/lifecycle.jsx
+++ my-react-app/src/KeyConcept/lifecycle.jsx
@@ -1,12 +1,69 @@
 import React from "react"
-import png from "./react-life-cycle.png"
+import png from "./react-life-cycle.jpeg"
 
 export default class lifecycle extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            count:10
+        }
+    }
+
+    componentWillMount(){
+        console.log("componentWillMount")
+    }
+    componentDidMount(){
+        console.log("componentDidMount")
+    }
+    shouldComponentUpdate(){
+        console.log("shouldComponentUpdate")
+        return true
+    }
+    componentWillUpdate(){
+        console.log("componentWillUpdate")
+    }
+    componentDidUpdate(){
+        console.log("componentDidUpdate")
+    }
+    componentWillReceiveProps(){
+        console.log("componentWillReceiveProps")
+    }
+    componentWillUnmount(){
+        console.log("componentWillUnmount")
+    }
+
+    increase = () => {
+        this.setState({
+            count: this.state.count += 1
+        })
+        console.log(this.state.count)
+    }
+
+    clickHandel=()=>{
+        this.props.child2parent("props子传父传参"); //data 传参
+    }
+
     render() {
+        const {count} = this.state
         return (
             <div class="container">
                 React生命周期函数 <br />
-                <img src={png}></img>
+                初始化props，初始化state <br />
+                componentWillMount(): 组件render()之前执行 <br/>
+                渲染函数render() <br/>
+                componentDidMount(): 组件render()之后执行 <br/>
+                shouldComponentUpdate(): 返回true和false，true允许改变 return true<br/>
+                componentWillUpdate(): 数据改变之前执行(state,props) <br/>
+                componentDidUpdate(): 数据修改完成(state,props) <br/>
+                componentWillReceiveProps(): props改变之前 <br/>
+                componentWillUnmount()：组件卸载前执行 <br/>
+                state修改：shouldComponentUpdate return true  <br/>
+                {count} <br/>
+                <button onClick={this.increase}>修改state</button> <br/>
+                {this.props.title} <br/>
+                props修改：(子传父传参) (注释App.jsx,倒数两个按钮)<br/>
+                <button onClick={this.clickHandel}>子传父修改props</button> <br/>
+                <img class="img-responsive" src={png} width={600}></img>
             </div>
         )
     }
